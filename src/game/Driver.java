7828801d98c6192f0005c1f9d1a1a3145c8a3705
Mike Wideman <wideman.mb@gmail.com>
2013-04-02 22:54:00 -0400

    Driver now interacts with non-stub Board/Entities

--- src/game/Driver.java
+++ src/game/Driver.java
@@ -9,11 +9,15 @@ import stub.StubPlayer;
 
 public class Driver {
 
+	static final int playerStartY = 600;
+	static final int playerStartX = 300;
 
+	
+	
 	public static void main(String[] args) {
-		Board board = new StubBoard();
-		Player p = new StubPlayer(board, new Point(200,200));
-		((StubBoard) board).addPlayer(p);
+		Board board = new Board();
+		board.createEntity(playerStartX, playerStartY, EntityTypes.PLAYER);
+		
 		GUI gui = new GUI(p,board);
 		gui.setVisible(true);
 
