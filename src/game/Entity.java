2f5226de82f774ddb3c94713858d6c1b9c75322b
Andrew Garrett <ajg9132@rit.edu>
2013-04-03 13:20:06 -0400

    move() should be private
    see comment in Entity

--- src/game/Entity.java
+++ src/game/Entity.java
@@ -7,6 +7,12 @@ import java.awt.geom.Point2D;
 
 public interface Entity extends Runnable{
 	
+	/*
+	 * move() should be private since every Runnable Entity is going to call
+	 * its move() over and over but no one else will - so no one should know
+	 * anything about move().
+	 */
+	//TODO: remove this method
 	public void move();
 	
 	public void collidesWith(Entity entity);
